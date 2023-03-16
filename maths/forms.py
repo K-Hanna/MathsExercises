0be1e1ce33e602e0c0b56ea9f6275d4f6029ddef
dhkot <dong_ha@wp.pl>
2023-03-16 19:24:06 +0100

    added tooltip and alignments

--- maths/forms.py
+++ maths/forms.py
@@ -10,8 +10,8 @@ CHOICES = (
 
 class MathsForm(forms.Form):
     amount = forms.IntegerField(min_value=1, max_value=50)
-    minValue = forms.IntegerField(min_value=0, max_value=50)
-    maxValue = forms.IntegerField(min_value=1, max_value=50)
+    minValue = forms.IntegerField(min_value=0, max_value=100)
+    maxValue = forms.IntegerField(min_value=1, max_value=100)
     operations = forms.MultipleChoiceField(
         choices=CHOICES,
         label="...",
